933a0907fb2fa68e51f28ea22c2848c277b2ba9e
Emmanuel Chucks <echucks19@gmail.com>
2023-11-13 07:08:19 +0000

    feat(api): add route handler

---
+++ app/api/names/route.ts
@@ -0,0 +1,17 @@
+const names = ["John", "Paul", "George", "Ringo"];
+
+export function GET(_request: Request) {
+  return Response.json(names);
+}
+
+export async function POST(request: Request) {
+  const formData = await request.formData();
+  const name = formData.get("name");
+
+  if (typeof name !== "string") {
+    return Response.json({ error: "Invalid name" }, { status: 400 });
+  }
+
+  names.push(name);
+  return Response.json(names);
+}
